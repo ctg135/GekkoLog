b050b8cbaf92dd21174be15bf6c7b902ecccbdb2
ctg135 <salabaevr@yandex.ru>
2025-06-01 18:07:29 +0300

    added flying buttons to home fragment

--- app/src/main/java/salabaev/gekkolog/ui/home/HomeFragment.kt
+++ app/src/main/java/salabaev/gekkolog/ui/home/HomeFragment.kt
@@ -1,19 +1,24 @@
 package salabaev.gekkolog.ui.home
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
+import salabaev.gekkolog.R
 import salabaev.gekkolog.data.GeckosDatabase
 import salabaev.gekkolog.data.event.EventRepository
 import salabaev.gekkolog.data.gecko.GeckoRepository
 import salabaev.gekkolog.data.reminder.ReminderRepository
 import salabaev.gekkolog.databinding.FragmentHomeBinding
 import java.util.Calendar
+import androidx.navigation.findNavController
 
 class HomeFragment : Fragment() {
 
@@ -104,9 +109,50 @@ class HomeFragment : Fragment() {
         calendar.set(Calendar.SECOND, 59)
         val endOfDay = calendar.timeInMillis
 
+        // Обработчики кликов по FAB
+        binding.fabAddEvent.setOnClickListener {
+            showEventTypeDialog()
+        }
+        binding.fabAddReminder.setOnClickListener {
+            val bundle = bundleOf(
+                "geckoId" to (arguments?.getInt("geckoId") ?: 0),
+                "reminderType" to "OTHER"
+            )
+            binding.root.findNavController()
+                .navigate(R.id.action_navigation_home_to_reminderFragment, bundle)
+        }
         // Load data
         viewModel.loadTodayNotifications(startOfDay, endOfDay)
         viewModel.loadTodayEvents(startOfDay, endOfDay)
+    }
+
+
+    private fun showEventTypeDialog() {
+
+
+
+        val eventTypes = resources.getStringArray(R.array.event_types)
+        val eventTypeValues = resources.getStringArray(R.array.event_type_values)
+
+        AlertDialog.Builder(requireContext())
+            .setTitle("Тип события?")
+            .setItems(eventTypes) { _, which ->
+
+                val selectedType = eventTypeValues[which]
+                val builder: AlertDialog.Builder = AlertDialog.Builder(context)
+                builder.apply {
+                    setMessage("Выберите тип события")
+                }
+                val bundle = bundleOf(
+                    "eventType" to selectedType,
+                    "eventId" to 0,
+                    "geckoId" to (arguments?.getInt("geckoId") ?: 0))
+                binding.root.findNavController()
+                    .navigate(R.id.action_navigation_home_to_eventFragment, bundle)
+            }
+            .setNegativeButton("Отмена", null)
+            .create()
+            .show()
     }
 
     override fun onDestroyView() {
